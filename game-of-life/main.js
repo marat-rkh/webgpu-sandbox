ed2ff2f4859a41d7f2542db6e45ba5588ca4b706
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2025-11-06 12:38:18 +0200

    First shaders

--- game-of-life/main.js
+++ game-of-life/main.js
@@ -20,6 +20,60 @@ context.configure({
     format: canvasFormat,
 });
 console.log("WebGPU successfully initialized.");
+const vertices = new Float32Array([
+    //   X,    Y,
+    -0.8, -0.8, // Triangle 1 (Blue)
+    0.8, -0.8,
+    0.8, 0.8,
+    -0.8, -0.8, // Triangle 2 (Red)
+    0.8, 0.8,
+    -0.8, 0.8,
+]);
+const vertexBuffer = device.createBuffer({
+    label: "Cell vertices",
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/ 0, vertices);
+const vertexBufferLayout = {
+    arrayStride: 8,
+    attributes: [{
+            format: "float32x2",
+            offset: 0,
+            shaderLocation: 0, // Position, see vertex shader
+        }],
+};
+const cellShaderModule = device.createShaderModule({
+    label: "Cell shader",
+    code: /* wgsl */ `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f) ->
+      @builtin(position) vec4f {
+      return vec4f(pos, 0, 1);
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+      return vec4f(1, 0, 0, 1); // (Red, Green, Blue, Alpha)
+    }
+  `
+});
+const cellPipeline = device.createRenderPipeline({
+    label: "Cell pipeline",
+    layout: "auto",
+    vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout]
+    },
+    fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+                format: canvasFormat
+            }]
+    }
+});
 const encoder = device.createCommandEncoder();
 const pass = encoder.beginRenderPass({
     colorAttachments: [{
@@ -29,6 +83,9 @@ const pass = encoder.beginRenderPass({
             storeOp: "store",
         }]
 });
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2); // 6 vertices
 pass.end();
 device.queue.submit([encoder.finish()]);
 export {};
