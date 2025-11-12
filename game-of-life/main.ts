c4f950f75c7b10e56a198ec79a7d11e22ea6c5af
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2025-11-12 18:58:32 +0200

    Cell state: compute shader stub and data layouts

--- game-of-life/main.ts
+++ game-of-life/main.ts
@@ -85,6 +85,61 @@ for (let i = 0; i < cellStateArray.length; i++) {
 }
 device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray);
 
+const bindGroupLayout = device.createBindGroupLayout({
+  label: "Cell Bind Group Layout",
+  entries: [{
+    binding: 0,
+    // Add GPUShaderStage.FRAGMENT here if you are using the `grid` uniform in the fragment shader.
+    visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
+    buffer: {} // Grid uniform buffer
+  }, {
+    binding: 1,
+    visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
+    buffer: { type: "read-only-storage"} // Cell state input buffer
+  }, {
+    binding: 2,
+    visibility: GPUShaderStage.COMPUTE,
+    buffer: { type: "storage"} // Cell state output buffer
+  }]
+});
+
+const bindGroups = [
+  device.createBindGroup({
+    label: "Cell renderer bind group A",
+    layout: bindGroupLayout, // Updated Line
+    entries: [{
+      binding: 0,
+      resource: { buffer: uniformBuffer }
+    }, {
+      binding: 1,
+      resource: { buffer: cellStateStorage[0] }
+    }, {
+      binding: 2,
+      resource: { buffer: cellStateStorage[1] }
+    }],
+  }),
+  device.createBindGroup({
+    label: "Cell renderer bind group B",
+    layout: bindGroupLayout, // Updated Line
+
+    entries: [{
+      binding: 0,
+      resource: { buffer: uniformBuffer }
+    }, {
+      binding: 1,
+      resource: { buffer: cellStateStorage[1] }
+    }, {
+      binding: 2,
+      resource: { buffer: cellStateStorage[0] }
+    }],
+  }),
+];
+
+const pipelineLayout = device.createPipelineLayout({
+  label: "Cell Pipeline Layout",
+  bindGroupLayouts: [ bindGroupLayout ],
+});
+
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
   code: /* wgsl */`
@@ -123,9 +178,35 @@ const cellShaderModule = device.createShaderModule({
   `
 });
 
+const WORKGROUP_SIZE = 8;
+// Create the compute shader that will process the simulation.
+const simulationShaderModule = device.createShaderModule({
+  label: "Game of Life simulation shader",
+  code: /*wgsl*/`
+    @group(0) @binding(0) var<uniform> grid: vec2f;
+
+    @group(0) @binding(1) var<storage> cellStateIn: array<u32>;
+    @group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
+
+    fn cellIndex(cell: vec2u) -> u32 {
+      return cell.y * u32(grid.x) + cell.x;
+    }
+
+    @compute 
+    @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
+    fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
+      if (cellStateIn[cellIndex(cell.xy)] == 1) {
+        cellStateOut[cellIndex(cell.xy)] = 0;
+      } else {
+        cellStateOut[cellIndex(cell.xy)] = 1;
+      }
+    }
+  `
+});
+
 const cellPipeline = device.createRenderPipeline({
   label: "Cell pipeline",
-  layout: "auto",
+  layout: pipelineLayout,
   vertex: {
     module: cellShaderModule,
     entryPoint: "vertexMain",
@@ -140,30 +221,15 @@ const cellPipeline = device.createRenderPipeline({
   }
 });
 
-const bindGroups = [
-  device.createBindGroup({
-    label: "Cell renderer bind group A",
-    layout: cellPipeline.getBindGroupLayout(0),
-    entries: [{
-      binding: 0,
-      resource: { buffer: uniformBuffer }
-    }, {
-      binding: 1,
-      resource: { buffer: cellStateStorage[0] }
-    }],
-  }),
-   device.createBindGroup({
-    label: "Cell renderer bind group B",
-    layout: cellPipeline.getBindGroupLayout(0),
-    entries: [{
-      binding: 0,
-      resource: { buffer: uniformBuffer }
-    }, {
-      binding: 1,
-      resource: { buffer: cellStateStorage[1] }
-    }],
-  })
-];
+// Create a compute pipeline that updates the game state.
+const simulationPipeline = device.createComputePipeline({
+  label: "Simulation pipeline",
+  layout: pipelineLayout,
+  compute: {
+    module: simulationShaderModule,
+    entryPoint: "computeMain",
+  }
+});
 
 const UPDATE_INTERVAL = 200; // Update every 200ms (5 times/sec)
 let step = 0; // Track how many simulation steps have been run
